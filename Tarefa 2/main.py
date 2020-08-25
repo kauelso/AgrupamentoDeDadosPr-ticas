cbc57db78c769b7835c273551aac1ef43efcea89
Kaue Oliveira <kauelucas@terra.com.br>
2020-08-25 19:32:19 -0300

    trabalho 4

--- Tarefa 2/main.py
+++ Tarefa 2/main.py
@@ -1,6 +1,31 @@
-import csv
+import pandas as pd
+import statistics
 
-with open('userprofile.csv','r') as arquivo:
-    reader = csv.DictReader(arquivo,delimiter=',')
-    for coluna in reader:
-        print(coluna['smoker'])
+
+def exclui_brancos(coluna):
+    for item in coluna:
+        if item == "?":
+            coluna.remove(item)
+    pass
+
+def replaced_itens(coluna,moda):
+    for i in range(0,len(coluna)):
+        if coluna[i] == "?":
+            coluna[i] = moda
+    pass
+
+input_name = "userprofile.csv"
+df = pd.read_csv(input_name)
+print(df)
+output_name = input_name[:-4] + '_output.csv'
+df_output = pd.DataFrame()
+for i in df.columns:
+    coluna = list(df[i])
+    aux = coluna.copy()
+    exclui_brancos(aux)
+    moda = statistics.mode(aux)
+    replaced_itens(coluna,moda)
+    df_output[i] = coluna
+df_output.to_csv(output_name)
+    
+    
