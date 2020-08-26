1ccba0fc3c0314091d6b022a43c0889c7c7ae0f4
laisdeghaide <laisdeghaide@ufu.br>
2020-08-25 21:07:06 -0300

    alteracao at4

--- Trabalho 4/atividade_4.py
+++ Trabalho 4/atividade_4.py
@@ -5,35 +5,11 @@ from completar_dados import *
 input_name = "iris.csv"
 df = pd.read_csv(input_name)
 
-#df.info()
-
-#df.head()
-
 #Removendo colunas que não são numéricas
 df.drop(df.select_dtypes(include=[object]), axis=1, inplace=True)
 df.dropna(inplace=True)
 
-#df.info()
-
-#Criando uma lista que contém as somas totais de cada coluna do conjunto de dados
-#e substituindo os valores marcados com "?" por NaN
-somas = []
-#teste
-for (nome_col, dados_col) in df.iteritems():
-  soma = 0
-  for (index, valor) in zip(df[nome_col].index, df[nome_col]):
-    if dados_col[index] == "?":
-      df[nome_col][index] = np.nan
-    else:
-      if not isinstance(valor, str):
-        valor = float(valor)
-        soma += valor
-  somas.append(soma)
-
-for i, (nome_col, dados_col) in enumerate(df.iteritems()):
-  for (index, valor) in zip(df[nome_col].index, df[nome_col]):
-    if dados_col[index] is np.nan:
-      if somas[i] == 0:
-        df[nome_col][index] = valor_mais_frequente(df, nome_col)
+#Imputando a média nos valores que estão faltando
+df.fillna(df.mean(), inplace=True)
 
 distancia_todos(df, 1)
--- Trabalho 4/completar_dados.py
+++ Trabalho 4/completar_dados.py
@@ -1,20 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def valor_mais_frequente(df, nome_col):
-  dic = {n: 0 for n in df[nome_col].unique()}
-  
-  for index, valor in df[nome_col].items():
-    for n in dic:
-      if n == valor:
-        dic[n] +=1
-
-  valor = sorted(dic.items(), reverse=True, key=itemgetter(1))[0][0]
-  return valor
-
 def distancia(obj1, obj2, dist=1):
-#  if dist != 1 and dist != 2:
-#    return 'Somente as distâncias 1 (euclediana) e 2 (manhattan) estão disponíveis!'
   
   lista = []
 
