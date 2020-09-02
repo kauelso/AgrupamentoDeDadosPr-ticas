1f34b9576f91da963618434ab0a7a456d9e7e67a
laisdeghaide <laisdeghaide@ufu.br>
2020-09-02 20:34:34 -0300

    initialcommit

--- Tarefa 6/at6.py
+++ Tarefa 6/at6.py
@@ -13,24 +13,5 @@ df = df[df.columns[:-1]]
 
 k = int(input("Numero de centroides: "))
 
-centroides = centroides(k,df)
-for i in range(0,100):
-    grupos = []
-    centroides_aux = []
-    for m in df.values:
-        distancias = []
-        for n in centroides:
-            distancias.append(distancia(m,n))
-        menor = min(distancias)
-        grupos[distancias.index(menor)] = m
-    for grp in range(centroides):
-        centroides_aux[grp] = np.mean(grupos[grp])
-    if centroides == centroides_aux:
-        break
-
 KMeans(k, df, max_interacao=100)
 
-
-
-
-#print(df.iloc[1])
--- Tarefa 6/funcoes.py
+++ Tarefa 6/funcoes.py
@@ -2,22 +2,12 @@ import pandas as pd
 import numpy as np
 import random
 
+
 def KMeans(k, df, max_interacao):
-  centroides = centroides(k, df)
-  print(centroides)
+  centroids = centroides(k, df)
+  agrup_ini = agrup_inicial(centroids, df)
+  print(agrup_ini)
 
-def distancia(obj1, obj2, dist=1):
-  
-  lista = []
-
-  for (valor1, valor2) in zip(obj1, obj2):
-    resultado = abs(valor1 - valor2) ** int(dist)
-    lista.append(resultado)
-
-  resultado = sum(lista)
-  resultado = resultado ** (1/int(dist))
-
-  return round(resultado, 2)
 
 def centroides(k, data):
   np.random.seed(200)
@@ -29,24 +19,35 @@ def centroides(k, data):
     if len(centroides) == k:
       return (centroides)
 
-def percorre(data, centros):
-  lista_dist = []
-  for i, (nome_col, dados_row) in enumerate(data.iteritems()):
-    for centro in enumerate(centros):
-      print(distancia(dados_row, centro, 1))
-      break
 
-def distancia_centroide(k, df):
-    centros = centroides(k,df)
-    for i in range(df.shape[0]):
-        distancias = []
-        inter =[]
-        for m in centros:
-            dist = distancia(df.iloc[i], df.iloc[m])
-            inter.append(dist)
-            valor_min = min(inter)
-            distancias.append(valor_min)
-            print(distancias)
+def distancia(obj1, obj2, dist=1):
+  lista = []
+
+  for (valor1, valor2) in zip(obj1, obj2):
+    resultado = abs(valor1 - valor2) ** int(dist)
+    lista.append(resultado)
+
+  resultado = sum(lista)
+  resultado = resultado ** (1/int(dist))
+
+  return round(resultado, 2)
+
+
+def agrup_inicial(centroides, df):
+  clusters = {n: 0 for n in range(0,df.shape[0])}
+  for i in range(0, df.shape[0]):
+    distancias = []
+    for e, m in enumerate(centroides):
+      dist = distancia(df.iloc[i], df.iloc[m])
+      distancias.append(dist)
+      if (e+1) == len(centroides):
+        menor = min(distancias)
+        for b, c in zip(distancias, centroides):
+          if menor == b:
+            clusters[i] = c
+            break
+  return clusters
+
 
 def output_txt(listas):
   f = open("output.txt", "w")
@@ -56,3 +57,25 @@ def output_txt(listas):
       f.write(','+ str(i))
     f.write("\n")
   f.close()
+
+
+#Talvez apagar
+#def distancia_centroide(k, df):
+#    centros = centroides(k,df)
+#    for i in range(df.shape[0]):
+#        distancias = []
+#        inter =[]
+#        for m in centros:
+#            dist = distancia(df.iloc[i], df.iloc[m])
+#            inter.append(dist)
+#            valor_min = min(inter)
+#            distancias.append(valor_min)
+#            print(distancias)
+
+
+#def percorre(data, centros):
+#  lista_dist = []
+#  for i, (nome_col, dados_row) in enumerate(data.iteritems()):
+#    for centro in enumerate(centros):
+#      print(distancia(dados_row, centro, 1))
+#      break
