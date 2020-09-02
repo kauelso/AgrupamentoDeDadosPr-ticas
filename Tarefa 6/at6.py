f0d196ca1362955d79ea2f3239c2b2cd60c500a6
Kaue Oliveira <kauelucas@terra.com.br>
2020-09-02 18:49:02 -0300

    tentando resolver esse trem

--- Tarefa 6/at6.py
+++ Tarefa 6/at6.py
@@ -12,6 +12,23 @@ df = df[df.columns[:-1]]
 #df.head()
 
 k = 3 #int(input("Numero de centroides: "))
+centroides = centroides(k,df)
+for i in range(0,100):
+    grupos = []
+    centroides_aux = []
+    for m in df.values:
+        distancias = []
+        for n in centroides:
+            distancias.append(distancia(m,n))
+        menor = min(distancias)
+        grupos[distancias.index(menor)] = m
+    for grp in range(centroides):
+        centroides_aux[grp] = np.mean(grupos[grp])
+    if centroides == centroides_aux:
+        break
 
 
-print(df.iloc[1])
+
+
+
+#print(df.iloc[1])
