e77016c7d3e9b0a1f1b0ccba69cae2d26a585c2b
laisdeghaide <laisdeghaide@ufu.br>
2020-09-12 19:08:50 -0300

    add tarefa 10

---
+++ Tarefa 10/at10.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import sys
+from completar_dados import *
+
+print(
+    "Insira abaixo o nome do arquivo que será utilizado, com extensão(exemplo.csv).\nEle deve estar na mesma pasta do programa."
+)
+
+input_name = input("Nome do arquivo: ")
+try:
+    df = pd.read_csv(input_name)
+except:
+    print("Arquivo não é um csv ou não foi encontrado")
+    sys.exit()
+
+
+# Imputando a média nos valores que estão faltando
+df.fillna(df.mean(), inplace=True)
+
+# print(acha_pos_min(distancia_todos(df)))
+print(acha_pos_min(distancia_todos(df)))
---
+++ Tarefa 10/completar_dados.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+
+
+def distancia(obj1, obj2):
+
+    lista = []
+
+    for (valor1, valor2) in zip(obj1, obj2):
+        resultado = abs(valor1 - valor2) ** 2
+        lista.append(resultado)
+
+    resultado = sum(lista)
+    resultado = resultado ** (1 / 2)
+
+    return round(resultado, 2)
+
+
+def distancia_todos(df):
+    listas = []
+    for i, (nome_col, dados_row) in enumerate(df.iterrows()):
+        lista = dados_row.values.tolist()
+        listas.append(lista)
+
+    resultado = []
+    for i, l1 in enumerate(listas):
+        r = []
+        for j, l2 in enumerate(listas[i:]):
+            result = distancia(l1, l2)
+            if result != 0.0:
+                r.append(result)
+        resultado.append(r)
+
+    return resultado
+
+
+def acha_pos_min(matriz):
+
+    # Retira listas vazias, caso houver
+    matriz_sem_vazio = [x for x in matriz if x != []]
+
+    # Dicionário que conterá o menor valor de cada linha
+    minimo = {}
+
+    for i, l1 in enumerate(matriz_sem_vazio):
+        linha = i
+        coluna = l1.index(min(l1))
+        menor_valor_cada_linha = min(l1)
+
+        # Se algum valor se repetir, pegamos o primeiro que aparece
+        if menor_valor_cada_linha not in minimo:
+            minimo[menor_valor_cada_linha] = [linha, coluna]
+
+    # Achando o mínimo dos minimos
+    chave = min(minimo.keys())
+    minn = minimo[chave]
+
+    return minn
+
+
+def output_txt(listas):
+    f = open("output.txt", "w")
+    for lista in listas:
+        f.write(str(lista[0]))
+        for i in lista[1:]:
+            f.write("," + str(i))
+        f.write("\n")
+    f.close()
