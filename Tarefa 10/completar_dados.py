fef195b9bb0c23228c584904f40be032cfb22c20
John Cunha <johnvitorc@gmail.com>
2020-09-16 20:45:08 -0300

    Update completar_dados.py

--- Tarefa 10/completar_dados.py
+++ Tarefa 10/completar_dados.py
@@ -25,8 +25,10 @@ def distancia_todos(df):
     resultado = []
     for i, l1 in enumerate(listas):
         r = []
-        for j, l2 in enumerate(listas[:i]):
+        for j, l2 in enumerate(listas):
             result = distancia(l1, l2)
+            if result == 0.0:
+              result = np.nan
             r.append(result)
         resultado.append(r)
 
