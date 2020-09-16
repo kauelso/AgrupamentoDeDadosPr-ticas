7aa73d18ef8c1302f918a0a9a996d249845d5a5e
Kaue Oliveira <kauelucas@terra.com.br>
2020-09-16 18:01:00 -0300

    coments

--- Tarefa 10/at10.py
+++ Tarefa 10/at10.py
@@ -14,14 +14,26 @@ from completar_dados import *
 #     print("Arquivo não é um csv ou não foi encontrado")
 #     sys.exit()
 
-df = pd.read_csv("iris.csv")
+df = pd.read_csv("teste.csv")
 
 #Computar a primeira matriz (pronto)
+matriz = distancia_todos(df)
 
+
+# print(matriz)
 #Encontrar menor valor (pronto)
-
+menor = acha_pos_min(df)
+print(matriz)
+# ind1 = menor[0]
+# ind2 = menor[1]
 #Unir os grupos do menor valor
-
+# for i in range(ind1,ind2):
+#     print(i)
+#     print(matriz[ind1][i])
+#     print(matriz[ind2][i])
+# matriz.remove(matriz[ind1])
+# print(matriz)
+        
 #Atualizar a matriz
 
 #Encerrrar somente quando houver apenas um grupo
@@ -30,7 +42,8 @@ df = pd.read_csv("iris.csv")
 
 
 # Imputando a média nos valores que estão faltando
-df.fillna(df.mean(), inplace=True)
+# df.fillna(df.mean(), inplace=True)
 
 # print(acha_pos_min(distancia_todos(df)))
-print(distancia_todos(df)[10][10])
+# print(distancia_todos(df))
+# print(cols_agrup(df,menor))
