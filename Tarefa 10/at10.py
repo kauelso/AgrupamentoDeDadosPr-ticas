81b9b743313cbf3901094b22d4c3f36da74025e4
Kaue Oliveira <kauelucas@terra.com.br>
2020-09-16 18:41:43 -0300

    teste de replace

--- Tarefa 10/at10.py
+++ Tarefa 10/at10.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sys
+import math
 from completar_dados import *
 
 # print(
@@ -14,7 +15,7 @@ from completar_dados import *
 #     print("Arquivo não é um csv ou não foi encontrado")
 #     sys.exit()
 
-df = pd.read_csv("teste.csv")
+df = pd.read_csv("iris.csv")
 
 #Computar a primeira matriz (pronto)
 matriz = distancia_todos(df)
@@ -23,10 +24,14 @@ matriz = distancia_todos(df)
 # print(matriz)
 #Encontrar menor valor (pronto)
 print(matriz)
-indice_menor_valor = np.unravel_index(np.nanargmin(df, axis=None), df.shape)
+indice_menor_valor = np.unravel_index(np.nanargmin(matriz, axis=None), matriz.shape)
 print(indice_menor_valor)
+ind1 = indice_menor_valor[0]
+ind2 = indice_menor_valor[1]
 #Unir os grupos do menor valor
-        
+for i,l in enumerate(matriz[ind2]):
+    matriz[i][ind2] = min(matriz[i][ind2],matriz[i][ind1])
+print(matriz)
 #Atualizar a matriz
 
 #Encerrrar somente quando houver apenas um grupo
