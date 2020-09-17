881133562ae964188ba3ffd14cab6e3b4f198836
Kaue Oliveira <kauelucas@terra.com.br>
2020-09-17 11:57:33 -0300

    inputs prontos

--- Tarefa 10/at10.py
+++ Tarefa 10/at10.py
@@ -4,31 +4,23 @@ import sys
 import math
 from completar_dados import *
 
-# print(
-#     "Insira abaixo o nome do arquivo que será utilizado, com extensão(exemplo.csv).\nEle deve estar na mesma pasta do programa."
-# )
+print(
+    "Insira abaixo o nome do arquivo que será utilizado, com extensão(exemplo.csv).\nEle deve estar na mesma pasta do programa."
+)
 
-# input_name = input("Nome do arquivo: ")
-# try:
-#     df = pd.read_csv(input_name)
-# except:
-#     print("Arquivo não é um csv ou não foi encontrado")
-#     sys.exit()
+input_name = input("Nome do arquivo: ")
+try:
+    df = pd.read_csv(input_name)
+except:
+    print("Arquivo não é um csv ou não foi encontrado")
+    sys.exit()
 
-df = pd.read_csv("teste.csv")
-
-#Computar a primeira matriz (pronto)
+#Computa a primeira matriz
 matriz = distancia_todos(df)
-
-
-# print(matriz)
-#Encontrar menor valor (pronto)
 print(matriz)
-while len(matriz) != 1:
-    single_link(matriz)
-    print(matriz)
-
-
-#Encerrrar somente quando houver apenas um grupo
-
-#Gerar output
+print("Escolha o tipo de output:")
+print("0 - Sem output de texto")
+print("1 - Apenas os grupos que foram formados a cada interacao")
+print("2 - Matriz inicial e grupos formados a cada interacao")
+output = int(input())
+single_link(matriz,output)
