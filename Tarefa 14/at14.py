71ecbd88bda9ddf98320e1c60b9713d8b41b9eac
Kaue Oliveira <kauelucas@terra.com.br>
2020-09-28 15:45:56 -0300

    perfeito

--- Tarefa 14/at14.py
+++ Tarefa 14/at14.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pandas
 import random
 import sys
 import numpy as np
@@ -8,27 +8,26 @@ from sklearn.cluster import KMeans
 
 print("Insira abaixo o nome do arquivo que será utilizado, com extensão(exemplo.csv).\nEle deve estar na mesma pasta do programa.")
 
-input_name = input("Nome do arquivo:")
-try:
-    df = pd.read_csv(input_name)
-except:
-    print("Arquivo não é um csv ou não foi encontrado")
-    sys.exit()
+# input_name = input("Nome do arquivo:")
+# try:
+#     df = pd.read_csv(input_name)
+# except:
+#     print("Arquivo não é um csv ou não foi encontrado")
+#     sys.exit()
+df = pd.read_csv("iris_com_classe.csv")
 
 k = int(input("Numero de centroides: "))
 
 
 # Convert DataFrame to matrix
-mat = dataset.values
+df_aux = df.copy()
+df_aux.drop(["variety"],axis=1,inplace= True)
+mat = df_aux.values
 # Using sklearn
-km = KMeans(n_clusters=3, max_iter=100, metric="euclidian")
+km = KMeans(n_clusters=k, max_iter=100)
 km.fit(mat)
 # Get cluster assignment labels
 labels = km.labels_
 # Format results as a DataFrame
-results = pandas.DataFrame([dataset.index, labels]).T
-
-
-kmeans = KMeans(n_clusters=3, max_iter=100, metrics="euclidian").fit(df)
-centroids = kmeans.cluster_centers_
-print(centroids)
+df["cluster"] = labels
+print(df)
